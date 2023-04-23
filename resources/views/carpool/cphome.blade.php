021965ce26343dace3db04312067af5b9310c53d
Pinwei <ch810538@gmail.com>
2023-04-23 08:53:54 +0800

    0423 0853

--- resources/views/carpool/cphome.blade.php
+++ resources/views/carpool/cphome.blade.php
@@ -5,6 +5,7 @@
 <title>共乘首頁</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{asset('css/carpoolcp.css')}}">
+    <link rel="stylesheet" href="{{asset('css/cphome.css')}}">
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     
 
